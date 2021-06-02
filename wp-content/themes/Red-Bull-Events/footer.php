4b250b19a89b1fb51e868c42c23bb7fa77c617e9
Lennert Verbesselt <lennert@verbesselt.net>
2021-06-02 16:05:11 +0200

    Added header navigation and footer with styling

--- wp-content/themes/Red-Bull-Events/footer.php
+++ wp-content/themes/Red-Bull-Events/footer.php
@@ -1,3 +1,21 @@
+
+<footer>
+
+<div class="container"> 
+    <div class="companyname">@2021 RED BULL EVENTS</div>
+        <?php 
+        wp_nav_menu(
+            array(
+                'theme_location' => 'footer-menu',
+                'menu_class' => 'footer-bar',
+            )
+        );?>
+    </div>
+    </footer>
+
+
+
+
 <?php wp_footer();?>
 </body>
 </html>
--- wp-content/themes/Red-Bull-Events/functions.php
+++ wp-content/themes/Red-Bull-Events/functions.php
@@ -29,6 +29,7 @@ register_nav_menus(
     array(
         'top-menu' => 'Top Menu Location',
         'mobile-menu' => 'Mobile Menu Location',
+        'footer-menu' => 'Footer Menu Location',
     )
 );
 
